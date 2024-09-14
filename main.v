38abb989c56495a1b7cb90715c4731081bbb449f
Kise K. <87082844+kisek@users.noreply.github.com>
2024-09-14 15:42:29 +0900

    Update main.v

--- main.v
+++ main.v
@@ -2,13 +2,13 @@
 `default_nettype none
 
 module m_main (
-    input  wire  w_CLK  ,
+    input  wire  w_clk  ,
     output wire  w_led1 ,
     output wire  w_led2
 );
 
     reg [31:0] r_cnt = 0;
-    always @(posedge w_CLK) r_cnt <= r_cnt + 1;
+    always @(posedge w_clk) r_cnt <= r_cnt + 1;
     assign w_led1 = r_cnt[23];
     assign w_led2 = r_cnt[24];
 endmodule
