98f59f1a0f82fbf5e833c894c6f15435c652e542
Kise K. <87082844+kisek@users.noreply.github.com>
2024-09-19 18:37:04 +0900

    Update main.v

--- main.v
+++ main.v
@@ -3,12 +3,10 @@
 
 module m_main (
     input  wire  w_clk  ,
-    output wire  w_led1 ,
-    output wire  w_led2
+    output wire [1:0] w_led
 );
 
     reg [31:0] r_cnt = 0;
     always @(posedge w_clk) r_cnt <= r_cnt + 1;
-    assign w_led1 = r_cnt[23];
-    assign w_led2 = r_cnt[24];
+    assign w_led = r_cnt[24:23];
 endmodule
